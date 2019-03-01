1d031beb01164e6c153a4c5b3b561e552b657744
GoncharenkoDanilo <goncharenko.danilo@gmail.com>
2019-03-01 18:12:29 +0200

    classes replaced

--- src/main/java/ProcRequest.java
+++ src/main/java/ProcRequest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.trainsmaven;
+
 
 import org.telegram.telegrambots.meta.api.objects.User;
 
